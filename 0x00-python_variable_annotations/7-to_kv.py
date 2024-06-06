8be44648880b4355b7c579bbe5510badd1c6b8ab
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-06 02:56:19 -0700

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuble[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function add two number"""
     return (k, float(v ** 2))
