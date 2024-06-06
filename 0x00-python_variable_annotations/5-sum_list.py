16af1960974ab7ce3e223503ab95c8e54b6b3872
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-06 02:48:28 -0700

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function add two number"""
+    return sum(input_list)
