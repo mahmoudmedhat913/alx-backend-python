42cadc2451d3cd25633837adfe67884ba421d4a4
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-06 02:51:47 -0700

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for function"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """function add two number"""
+    return sum(mxd_lst)
